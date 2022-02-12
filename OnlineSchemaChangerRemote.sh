fea69958f454b2de7e7fe520ea61eec8eb0e5201
oracle4you <96489676+oracle4you@users.noreply.github.com>
2022-02-13 00:35:29 +0200

    Update OnlineSchemaChangerRemote.sh

--- OnlineSchemaChangerRemote.sh
+++ OnlineSchemaChangerRemote.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
- pt-online-schema-change --alter ' ADD COLUMN myNumber int not null default 0' D=izzy,t=test1 --host 10.70.1.4 --user root --password root --alter-foreign-keys-method auto --max-load Threads_running=150 --critical-load Threads_running=300 --chunk-size 1000 --chunk-time 0.5 --chunk-size-limit 4.0 --dry-run --print
+ pt-online-schema-change --alter ' ADD COLUMN myNumber int not null default 0' D=izzy,t=test1 --host 1.1.1.1 --user root --password root --alter-foreign-keys-method auto --max-load Threads_running=150 --critical-load Threads_running=300 --chunk-size 1000 --chunk-time 0.5 --chunk-size-limit 4.0 --dry-run --print
